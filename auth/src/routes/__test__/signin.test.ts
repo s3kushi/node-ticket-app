6034f88a1afee5cefc1309fd48b9ac8537920f67
s3kushi <daniel.756@hotmail.com>
2021-01-19 17:43:15 -0300

    Finishing up auth service. Added tests suite and created an app file. Fixed styling/linting config

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('fails when email does not exist', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('fails when incorrect password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'ddddddddddd',
+    })
+    .expect(400);
+});
+
+it('responds with a cookie', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
