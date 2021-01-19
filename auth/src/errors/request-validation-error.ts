88f889d049b6e46552bfb63dbf285905fd11d64e
s3kushi <daniel.756@hotmail.com>
2021-01-19 00:20:07 -0300

    Added routes, linting config, error classes and handler, mongoose user model and password hashing, docker and k8s files

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,16 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+
+  constructor(private errors: ValidationError[]) {
+    super('Invalid parameters.');
+
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map((err) => ({ message: err.msg, field: err.param }));
+  }
+}
